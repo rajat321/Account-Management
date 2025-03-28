9a05e2ca6a6861f69e49eb3fda4c8e0210120496
rajat321 <41101625+rajat321@users.noreply.github.com>
2025-03-28 16:33:59 +0530

    Add files via upload

--- account_management.sql
+++ account_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2025 at 11:48 AM
+-- Generation Time: Mar 28, 2025 at 12:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,22 +33,12 @@ CREATE TABLE `accounts` (
   `account_name` varchar(255) NOT NULL,
   `account_number` varchar(255) NOT NULL,
   `account_type` enum('Personal','Business') NOT NULL,
-  `currency` enum('USD','EUR','GBP','INR') NOT NULL,
+  `currency` enum('USD','EUR','GBP') NOT NULL,
   `balance` decimal(15,2) NOT NULL DEFAULT 0.00,
   `deleted_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `accounts`
---
-
-INSERT INTO `accounts` (`id`, `user_id`, `account_name`, `account_number`, `account_type`, `currency`, `balance`, `deleted_at`, `created_at`, `updated_at`) VALUES
-(1, 1, 'sedsad', '3260709625401', 'Business', 'EUR', 600.00, NULL, '2025-03-27 07:01:08', '2025-03-27 09:36:27'),
-(2, 2, 'My Testing Account', '1781430169288', 'Personal', 'USD', 0.00, NULL, '2025-03-27 10:48:01', '2025-03-27 11:46:03'),
-(3, 2, 'new tgestasdsa', '6140599117635', 'Business', 'EUR', 100.50, '2025-03-27 11:20:59', '2025-03-27 11:07:13', '2025-03-27 11:20:59'),
-(4, 2, 'New Check Account', '8221113750859', 'Business', 'EUR', 201.00, NULL, '2025-03-27 23:29:25', '2025-03-28 00:16:05');
 
 -- --------------------------------------------------------
 
@@ -180,21 +170,6 @@ CREATE TABLE `personal_access_tokens` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `personal_access_tokens`
---
-
-INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
-(1, 'App\\Models\\User', 1, 'auth_token', '058f26be11c8a0cae89ace60e89ced3d594164ac778d7b4217449abe64dd4f3c', '[\"*\"]', NULL, NULL, '2025-03-27 06:48:12', '2025-03-27 06:48:12'),
-(2, 'App\\Models\\User', 1, 'auth_token', 'f36069a7b1e52632d3497aeef3385a7480d110a348bdc9026927884042dd1822', '[\"*\"]', '2025-03-27 09:41:40', NULL, '2025-03-27 06:50:20', '2025-03-27 09:41:40'),
-(3, 'App\\Models\\User', 2, 'auth_token', 'a788ce8147e60c0c12042ad1515e0fbbdb59f1621587d9db068cabb4510cd7e8', '[\"*\"]', NULL, NULL, '2025-03-27 10:46:40', '2025-03-27 10:46:40'),
-(4, 'App\\Models\\User', 2, 'auth_token', 'e1a86d956a7f5124620b0971dc14df32c894769e5009332e3776c77ca8d9718a', '[\"*\"]', '2025-03-27 10:51:16', NULL, '2025-03-27 10:47:17', '2025-03-27 10:51:16'),
-(5, 'App\\Models\\User', 2, 'auth_token', '8fd4fb219985eda7be149a5ec747018fc2ae210f847cf09c052d8e3417ab09df', '[\"*\"]', NULL, NULL, '2025-03-27 10:56:34', '2025-03-27 10:56:34'),
-(6, 'App\\Models\\User', 2, 'auth_token', '9f4b436372b1471df8c46d27e9ca5919a6df37520d8fccfba799479b4dd9b32d', '[\"*\"]', '2025-03-27 11:46:03', NULL, '2025-03-27 11:04:00', '2025-03-27 11:46:03'),
-(7, 'App\\Models\\User', 2, 'auth_token', '2c6bbd5c783b2012af100af56962afc532a63c62685a9e0e55061239ea2feaea', '[\"*\"]', '2025-03-27 23:43:55', NULL, '2025-03-27 23:28:42', '2025-03-27 23:43:55'),
-(8, 'App\\Models\\User', 2, 'auth_token', 'f8635ff3d3d05c91a77a567ea01708867cd69a9541d5b21e60d8beae83bd5b60', '[\"*\"]', '2025-03-28 00:16:05', NULL, '2025-03-28 00:13:32', '2025-03-28 00:16:05'),
-(9, 'App\\Models\\User', 2, 'auth_token', 'b25ddd04b4d743d33a4cc4278a42ec26fffb8c2a63b436156df1b61326ef1bad', '[\"*\"]', NULL, NULL, '2025-03-28 01:45:40', '2025-03-28 01:45:40');
-
 -- --------------------------------------------------------
 
 --
@@ -210,15 +185,6 @@ CREATE TABLE `sessions` (
   `last_activity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `sessions`
---
-
-INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('9sZzi1k6LANoHtuFSXUpXwQtamJsjWrFfbUkV2yg', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiMDRqdTZmYlpmajFMMVFNVlJSZ2E5VEp0b09FcDcyQW1EZGp5ZlI3ZCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1743077486),
-('h0UewUERfNS8gqvvpPLNNirodoNfY5G8OzJjo6Gp', NULL, '127.0.0.1', 'PostmanRuntime/7.43.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiWHVTRHMwNlJtTm1SYXBLSHYxNTZIc3R3ZG1iVFdvU2NYUVl1RjJEVyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1743078231),
-('SOuHcyzaSyHGtrOJNEqxS8OO51YxtD7lPru3E1iB', NULL, '127.0.0.1', 'PostmanRuntime/7.43.2', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiVlkwcTZYbU1MengyMlpYUEtycndzdEVZaHhOWTZCSEI0NUpiUnRqRCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1743088183);
-
 -- --------------------------------------------------------
 
 --
@@ -226,24 +192,14 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 --
 
 CREATE TABLE `transactions` (
-  `id` int(11) NOT NULL,
-  `account_id` bigint(20) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `account_id` bigint(20) UNSIGNED NOT NULL,
   `type` enum('Credit','Debit') NOT NULL,
-  `amount` decimal(10,2) NOT NULL,
-  `description` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `amount` decimal(15,2) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `transactions`
---
-
-INSERT INTO `transactions` (`id`, `account_id`, `type`, `amount`, `description`, `created_at`, `updated_at`) VALUES
-(1, 2, 'Credit', 1000.00, 'Payment for services', '2025-03-27 11:45:24', '2025-03-27 11:45:24'),
-(2, 2, 'Debit', 1100.00, 'Payment for services', '2025-03-27 11:46:03', '2025-03-27 11:46:03'),
-(3, 4, 'Credit', 100.50, 'Salary Deposit', '2025-03-28 00:15:59', '2025-03-28 00:15:59'),
-(4, 4, 'Credit', 100.50, 'Salary Deposit', '2025-03-28 00:16:05', '2025-03-28 00:16:05');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -261,14 +217,6 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'John Doe', 'john@example.com', NULL, '$2y$12$zslj50iwo9OWaQXSJVrTVuFpcsFMT1FWsw6ZHn57VdPhR1nVIDu.C', 'i15HytpOlTCR1Utj9zdnkAk6VtsvburHzb0GC8Hqj2RioBDuatZnLin2CWkj', '2025-03-27 06:48:12', '2025-03-27 06:48:12'),
-(2, 'Rajat Sharma', 'rajat@gmail.com', NULL, '$2y$12$naYip8Det3XEIkfFb6daye9yYp.Q9v2W7MVq2Hu2NBuQHUr18gP0m', 'YsZF1unm18xQC07nzgifCEcKkY5m9IYUB88s2hu0CcEWSNQX1bthPMd0KtRc', '2025-03-27 10:46:40', '2025-03-27 10:46:40');
 
 --
 -- Indexes for dumped tables
@@ -347,7 +295,8 @@ ALTER TABLE `sessions`
 -- Indexes for table `transactions`
 --
 ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `transactions_account_id_foreign` (`account_id`);
 
 --
 -- Indexes for table `users`
@@ -364,7 +313,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -388,19 +337,19 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -411,6 +360,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `accounts`
   ADD CONSTRAINT `accounts_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_account_id_foreign` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
